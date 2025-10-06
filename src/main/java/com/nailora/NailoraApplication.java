8b1e49bf7bcac14d73a43c0139d9d11d2fdae7d3
Thanaruk598-0 <Thanaruk.su@kkumail.com>
2025-10-07 01:42:16 +0700

    Initial commit

---
+++ src/main/java/com/nailora/NailoraApplication.java
@@ -0,0 +1,13 @@
+package com.nailora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NailoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NailoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nailora/entity/NailoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nailora.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NailoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
